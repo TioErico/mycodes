8ca5de1a1e895f8eeaee532190a5c445090918fe
Tio Erico <90931355+TioErico@users.noreply.github.com>
2024-09-18 13:42:16 -0300

    Atualizar o piscaled.ino

--- piscaled.ino
+++ piscaled.ino
@@ -1,9 +1,11 @@
-// Pisca led// Pisca led básico
+// Pisca led básico
 
-int led = 12; //cria a variável "led" e atribui o valor 12 a ela (porta 12)
+int led = 12; 
+//cria a variável e atribui o valor 12 a ela
 
 void setup() {
-  pinMode(led, OUTPUT); //define o led (porta 12) como saída
+  pinMode(led, OUTPUT); 
+//define o led (porta 12) como saída
 }
 
 void loop() {
